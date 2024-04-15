94ab7a58e089e8ff272dab5d9a26cf9156f80dad
Thilina Madushan <satmadushan97@gmail.com>
2024-04-16 00:16:08 +0530

    Added inheritance

---
+++ OOP_Inheritance/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Inheritance
+{
+    internal class Pet
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Walk()
+        {
+            Console.WriteLine("Pet is walking");
+        }
+
+    }
+}
--- OOP_Inheritance/Program.cs
+++ OOP_Inheritance/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using OOP_Inheritance;
+
+var myCat = new Cat();
+
+myCat.Name = "Kitty";
+myCat.Age = 2;
+myCat.Color = "White";
+
+myCat.Walk();
+
+var myDog  = new Dog();
+
+myDog.Name = "Browny";
+myDog.Age = 3;
+myDog.Color = "Brown";
+
+myDog.Walk();
